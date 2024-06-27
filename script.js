abbd8df025b30948ff75ebdb4a05b8d5d5c3f167
bahanangeorge <bahanangeorge25@gmail.com>
2024-06-27 12:45:25 +0530

    new commit

--- script.js
+++ script.js
@@ -1,100 +1,141 @@
-//console.log("hello world");
-//console.warn("hello world");
-//console.error("hello world");
+// console.log("hello world");
+// console.warn("hello world");
+// console.error("hello world");
 
 
-//var a = 30;
-//console.log(a);
-//var a = "Name";
-//console.log(a);
+// var a = 30;
+// console.log(a);
+// var a = "Name";
+// console.log(a);
 
-//let name = "john";
-//console.log(name);
-//let name = "sam";
-
-
-//const pi = 3.14;
-//console.log(pi);
-//const pi=3.145;
-//console.log(pi);
-
-//let valueOne = 2;
-//let valueTwo = "a";
-//console.log(valueOne +parseInt(valueTwo));
-
-//const name = "john";
-//const age = 30;
-//const rating = 4.5;
-//const isCool = true;
-//const x = null;
-//var y;
-//console.log(typeof name);
-
-//console.log('My name is' + name +' and I am ' +age);
-//console.log("My name is" + name +" and I am " +age);
-//console.log("My name is $(name) + and I am $(age)");
-
-//Array
-
-//arrayOne = ["john",30,true,null,undefined]
-//console.log(arrayOne);
-
-//arrayOne.push("Apple");
-//console.log(arrayOne);
-//arrayOne.pop();
-//console.log(arrayOne);
-
-//unshift
-
-//arrayOne.unshift("Mango");
-//console.log(arrayOne);
+// //let name = "john";
+// //console.log(name);
+// //let name = "sam";
 
 
+// const pi = 3.14;
+// console.log(pi);
+// //const pi=3.145;
+// //console.log(pi);
+
+// let valueOne = 2;
+// let valueTwo = "a";
+// console.log(valueOne +parseInt(valueTwo));
+
+// const name = "john";
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// var y;
+// console.log(typeof name);
+
+// console.log('My name is' + name +' and I am ' +age);
+// console.log("My name is" + name +" and I am " +age);
+// console.log("My name is $(name) + and I am $(age)");
+
+// //Array
+
+// arrayOne = ["john",30,true,null,undefined]
+// console.log(arrayOne);
+
+// arrayOne.push("Apple");
+// console.log(arrayOne);
+// arrayOne.pop();
+// console.log(arrayOne);
+
+// //unshift
+
+// arrayOne.unshift("Mango");
+// console.log(arrayOne);
+
+// console.log(arrayOne[0]);
+
+// arrayOne[3]="banana"
+// console.log(arrayOne);
+
+// console.log(arrayOne.length);
+
+// arrayOne.splice(3,1);
+// console.log(arrayOne);
+
+// let valueOnee="2";
+// let valueTwoo=2;
+
+// console.log(valueOnee==valueTwoo);
+// console.log(valueOnee=== valueTwoo);
+
+// //conditional statements
+
+// let value= 18;
+// if(value>=18)
+//     {
+//         console.log("you are adult");
+//     }
+
+// //if else
+
+// let value1= 18;
+// if(value1>=18)
+//     {
+//         console.log("you are adult");
+//     }
+// else{
+//     console.log("you are not adult");
+// }
+
+// //Ternary
+
+// let ages=20;
+
+// let type = (age>=18)?"Adult" :"Minor"
+// console.log(type);
 
 
+//Function
 
-//Day3
-//let r = 5;
-//let y = 2;
-//let z = x + y;
+console.log("Functions");
 
-//console.log(z);
+function showString(){
+    console.log("hello world");
+}
 
-console.log("Function");
+showString();
 
-function showString()
+var showString = function()
 {
     console.log("hello world");
 }
-showString()
+showString();
+
+//arrow Function
+
+
+var showString =() =>
 {
-    var showstring=function(){
-
-    }
+    console.log("Arrow function hello world");
 }
+showString(); 
 
-
-
-
-//map
+//map Function
 
 var number=[1,2,3,4,5];
 
-var square=number.map((num)->{
+var square = number.map((num)=>{
     return num*num;
 })
-
-
 console.log(square);
 
-var fruits = ["apple","banana"];
-var upperCase = fruits.map((fruits)->{
-    return fruit.topperCase();
 
-});
+var fruits=["Apple","Banana"];
 
+var upperCase = fruits.map((fruit)=>{
+    return fruit.toUpperCase;
+})
 console.log(upperCase);
 
+
+//enhanced object
 
 var name="john";
 var age=20;
@@ -102,43 +143,39 @@ var age=20;
 var person = {
     name,
     age,
-    isStudent:true,
+    isStudent:true
 }
 
 console.log(person);
 
-
-
 //Destructuring
 
 var person={
-    name:Akhil,
+    name:"adith",
     age:20,
-    isStudent:true,
-
+    isStudent:true
 }
+
+var {name ,age, isStudent}=person
 
 console.log(name);
 console.log(age);
 console.log(isStudent);
 
-
 //promises
 
-var promise=new promise((resolve,reject)=>{
-   setTimeout(()=>{
-      resolve("promise resolved")
 
-   }, 2000);
-})
+// var promise= new Promise((resolve,reject)=>{
+//     setTimerout(()=>{
+//          resolve("Promise resolved")
+//     },2000)
+// })
 
-promise.then((result)=>{
-    "result":unknown word
-})
+// promise.then((result)=>{
+//     console.log(result);
+// })
 
-
-
-//! spread opeerator
+//Spread operator
 
 var numbers=[1,2,3,4,5];
 
